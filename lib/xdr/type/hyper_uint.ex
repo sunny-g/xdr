b8c9a6d53b9c3889cff50e2886db8038b821a188
Sunny G <sunny.gonna@gmail.com>
2017-09-04 02:12:50 -0700

    updates HyperUint docs, types and tests

--- lib/xdr/type/hyper_uint.ex
+++ lib/xdr/type/hyper_uint.ex
@@ -1,28 +1,47 @@
 defmodule XDR.Type.HyperUint.Validation do
   require Math
 
-  @min_int 0
-  @max_int Math.pow(2, 64) - 1
+  @min_hyper_uint 0
+  @max_hyper_uint Math.pow(2, 64) - 1
 
   defmacro is_valid?(int) do
     quote do
       is_integer(unquote(int))
-      and unquote(int) >= unquote(@min_int)
-      and unquote(int) <= unquote(@max_int)
+      and unquote(int) >= unquote(@min_hyper_uint)
+      and unquote(int) <= unquote(@max_hyper_uint)
     end
   end
 end
 
 defmodule XDR.Type.HyperUint do
-  import XDR.Type.HyperUint.Validation
+  require XDR.Type.HyperUint.Validation
 
-  def encode(int) when not is_integer(int), do: {:error, :invalid}
-  def encode(int) when not is_valid?(int), do: {:error, :out_of_bounds}
-  def encode(int), do: {:ok, <<int :: unsigned-size(64)>>}
+  @typedoc """
+  Hyper unsigned integer between 0 and 2^64 - 1
+  """
+  @type t :: 0..18446744073709551615
 
+  @doc """
+  Determines if a value is a valid 8-byte hyper unsigned integer
+  """
+  @spec is_valid?(__MODULE__.t) :: boolean
+  def is_valid?(hyper_uint), do: XDR.Type.HyperUint.Validation.is_valid?(hyper_uint)
+
+  @doc """
+  Encodes a hyper unsigned integer into an 8-byte binary
+  """
+  @spec encode(__MODULE__.t) :: {:ok, <<_ :: 64>>} | {:error, :invalid | :out_of_bounds}
+  def encode(hyper_uint) when not is_integer(hyper_uint), do: {:error, :invalid}
+  def encode(hyper_uint) when not XDR.Type.HyperUint.Validation.is_valid?(hyper_uint), do: {:error, :out_of_bounds}
+  def encode(hyper_uint), do: {:ok, <<hyper_uint :: unsigned-size(64)>>}
+
+  @doc """
+  Decodes a 8-byte binary into a hyper unsigned integer
+  """
+  @spec decode(<<_ :: 64>>) :: {:ok, __MODULE__.t} | {:error, :invalid | :out_of_bounds}
   def decode(xdr) when not is_binary(xdr), do: {:error, :invalid}
   def decode(xdr) when bit_size(xdr) > 64, do: {:error, :out_of_bounds}
-  def decode(<<int :: unsigned-size(64)>>) when not is_integer(int), do: {:error, :invalid}
-  def decode(<<int :: unsigned-size(64)>>), do: {:ok, int}
+  def decode(<<hyper_uint :: unsigned-size(64)>>) when not is_integer(hyper_uint), do: {:error, :invalid}
+  def decode(<<hyper_uint :: unsigned-size(64)>>), do: {:ok, hyper_uint}
   def decode(_), do: {:error, :invalid}
 end
