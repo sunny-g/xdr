e3ce81ccc6fc32af5d130bb7f88934fb9467a879
Sunny G <sunny.gonna@gmail.com>
2017-09-04 02:07:30 -0700

    updates Hyper Int docs, types and tests

--- lib/xdr/type/hyper_int.ex
+++ lib/xdr/type/hyper_int.ex
@@ -1,25 +1,44 @@
 defmodule XDR.Type.HyperInt.Validation do
   require Math
 
-  @min_int -Math.pow(2, 63)
-  @max_int Math.pow(2, 63) - 1
+  @min_hyper_int -Math.pow(2, 63)
+  @max_hyper_int Math.pow(2, 63) - 1
 
   defmacro is_valid?(hyper_int) do
     quote do
       is_integer(unquote(hyper_int))
-      and unquote(hyper_int) >= unquote(@min_int)
-      and unquote(hyper_int) <= unquote(@max_int)
+      and unquote(hyper_int) >= unquote(@min_hyper_int)
+      and unquote(hyper_int) <= unquote(@max_hyper_int)
     end
   end
 end
 
 defmodule XDR.Type.HyperInt do
-  import XDR.Type.HyperInt.Validation
+  require XDR.Type.HyperInt.Validation
 
+  @typedoc """
+  Hyper integer between -2^63 to 2^63 - 1
+  """
+  @type t :: -9223372036854775808..9223372036854775807
+
+  @doc """
+  Determines if a value is a valid 8-byte hyper integer
+  """
+  @spec is_valid?(__MODULE__.t) :: boolean
+  def is_valid?(hyper_int), do: XDR.Type.HyperInt.Validation.is_valid?(hyper_int)
+
+  @doc """
+  Encodes a hyper integer into an 8-byte binary
+  """
+  @spec encode(__MODULE__.t) :: {:ok, <<_ :: 64>>} | {:error, :invalid | :out_of_bounds}
   def encode(hyper_int) when not is_integer(hyper_int), do: {:error, :invalid}
-  def encode(hyper_int) when not is_valid?(hyper_int), do: {:error, :out_of_bounds}
+  def encode(hyper_int) when not XDR.Type.HyperInt.Validation.is_valid?(hyper_int), do: {:error, :out_of_bounds}
   def encode(hyper_int), do: {:ok, <<hyper_int :: signed-size(64)>>}
 
+  @doc """
+  Decodes an 8-byte binary into a hyper integer
+  """
+  @spec decode(<<_ :: 64>>) :: {:ok, __MODULE__.t} | {:error, :invalid | :out_of_bounds}
   def decode(xdr) when not is_binary(xdr), do: {:error, :invalid}
   def decode(xdr) when bit_size(xdr) > 64, do: {:error, :out_of_bounds}
   def decode(<<hyper_int :: signed-size(64)>>) when not is_integer(hyper_int), do: {:error, :invalid}
--- test/xdr/type/hyper_int_test.exs
+++ test/xdr/type/hyper_int_test.exs
@@ -1,53 +1,52 @@
 defmodule XDR.Type.HyperIntTest do
   use ExUnit.Case
   require Math
-  import XDR.Type.HyperInt.Validation
   alias XDR.Type.HyperInt
   doctest XDR.Type.HyperInt
 
-  @min_int -Math.pow(2, 63)
-  @max_int Math.pow(2, 63) - 1
+  @min_hyper_int -Math.pow(2, 63)
+  @max_hyper_int Math.pow(2, 63) - 1
 
   test "is_valid?" do
-    assert is_valid?(0) == true
-    assert is_valid?(1) == true
-    assert is_valid?(-1) == true
-    assert is_valid?(@min_int) == true
-    assert is_valid?(@max_int) == true
+    assert HyperInt.is_valid?(0) == true
+    assert HyperInt.is_valid?(1) == true
+    assert HyperInt.is_valid?(-1) == true
+    assert HyperInt.is_valid?(@min_hyper_int) == true
+    assert HyperInt.is_valid?(@max_hyper_int) == true
 
-    assert is_valid?(0.0) == false
-    assert is_valid?(-0.1) == false
-    assert is_valid?(-:math.pow(2, 63)) == false
-    assert is_valid?(:math.pow(2, 63) - 1) == false
-    assert is_valid?(@min_int - 1) == false
-    assert is_valid?(@max_int + 1) == false
-    assert is_valid?(true) == false
-    assert is_valid?(false) == false
-    assert is_valid?(nil) == false
-    assert is_valid?("0") == false
-    assert is_valid?({}) == false
-    assert is_valid?([]) == false
-    assert is_valid?([0]) == false
+    assert HyperInt.is_valid?(0.0) == false
+    assert HyperInt.is_valid?(-0.1) == false
+    assert HyperInt.is_valid?(-:math.pow(2, 63)) == false
+    assert HyperInt.is_valid?(:math.pow(2, 63) - 1) == false
+    assert HyperInt.is_valid?(@min_hyper_int - 1) == false
+    assert HyperInt.is_valid?(@max_hyper_int + 1) == false
+    assert HyperInt.is_valid?(true) == false
+    assert HyperInt.is_valid?(false) == false
+    assert HyperInt.is_valid?(nil) == false
+    assert HyperInt.is_valid?("0") == false
+    assert HyperInt.is_valid?({}) == false
+    assert HyperInt.is_valid?([]) == false
+    assert HyperInt.is_valid?([0]) == false
   end
 
   test "encode" do
     assert HyperInt.encode(0) == {:ok, <<0, 0, 0, 0, 0, 0, 0, 0>>}
     assert HyperInt.encode(1) == {:ok, <<0, 0, 0, 0, 0, 0, 0, 1>>}
     assert HyperInt.encode(-1) == {:ok, <<255, 255, 255, 255, 255, 255, 255, 255>>}
-    assert @min_int |> HyperInt.encode == {:ok, <<128, 0, 0, 0, 0, 0, 0, 0>>}
-    assert @max_int |> HyperInt.encode == {:ok, <<127, 255, 255, 255, 255, 255, 255, 255>>}
+    assert @min_hyper_int |> HyperInt.encode == {:ok, <<128, 0, 0, 0, 0, 0, 0, 0>>}
+    assert @max_hyper_int |> HyperInt.encode == {:ok, <<127, 255, 255, 255, 255, 255, 255, 255>>}
 
     assert HyperInt.encode(0.1) == {:error, :invalid}
-    assert @min_int - 1 |> HyperInt.encode == {:error, :out_of_bounds}
-    assert @max_int + 1 |> HyperInt.encode == {:error, :out_of_bounds}
+    assert @min_hyper_int - 1 |> HyperInt.encode == {:error, :out_of_bounds}
+    assert @max_hyper_int + 1 |> HyperInt.encode == {:error, :out_of_bounds}
   end
 
   test "decode" do
     assert HyperInt.decode(<<0, 0, 0, 0, 0, 0, 0, 0>>) == {:ok, 0}
     assert HyperInt.decode(<<0, 0, 0, 0, 0, 0, 0, 1>>) == {:ok, 1}
     assert HyperInt.decode(<<255, 255, 255, 255, 255, 255, 255, 255>>) == {:ok, -1}
-    assert HyperInt.decode(<<128, 0, 0, 0, 0, 0, 0, 0>>) == {:ok, @min_int}
-    assert HyperInt.decode(<<127, 255, 255, 255, 255, 255, 255, 255>>) == {:ok, @max_int}
+    assert HyperInt.decode(<<128, 0, 0, 0, 0, 0, 0, 0>>) == {:ok, @min_hyper_int}
+    assert HyperInt.decode(<<127, 255, 255, 255, 255, 255, 255, 255>>) == {:ok, @max_hyper_int}
 
     assert HyperInt.decode("0") == {:error, :invalid}
     assert HyperInt.decode(<<0, 0, 0, 0, 0, 0, 0, 0, 0>>) == {:error, :out_of_bounds}
