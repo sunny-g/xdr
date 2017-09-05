1ed786fa5afc1ea2d18f3088cd0cb7b6588cc11d
Sunny G <sunny.gonna@gmail.com>
2017-09-04 23:24:16 -0700

    update bool types

--- lib/xdr/type/bool.ex
+++ lib/xdr/type/bool.ex
@@ -9,7 +9,7 @@ defmodule XDR.Type.Bool do
   @doc """
   Determines if value is a valid boolean
   """
-  @spec is_valid?(__MODULE__.t) :: boolean
+  @spec is_valid?(bool :: __MODULE__.t) :: boolean
   def is_valid?(false), do: true
   def is_valid?(true), do: true
   def is_valid?(_), do: false
@@ -17,7 +17,7 @@ defmodule XDR.Type.Bool do
   @doc """
   Encodes a boolean value into 4-byte binary - true maps to 1, false maps to 0
   """
-  @spec encode(__MODULE__.t) :: {:ok, <<_ :: 32>>} | {:error, :invalid}
+  @spec encode(bool :: __MODULE__.t) :: {:ok, xdr :: <<_ :: 32>>} | {:error, :invalid}
   def encode(false), do: Int.encode(0)
   def encode(true), do: Int.encode(1)
   def encode(_), do: {:error, :invalid}
@@ -25,7 +25,7 @@ defmodule XDR.Type.Bool do
   @doc """
   Decodes a 4-byte binary into a boolean
   """
-  @spec decode(<<_ :: 32>>) :: {:ok, __MODULE__.t} | {:error, :invalid}
+  @spec decode(xdr :: <<_ :: 32>>) :: {:ok, bool :: __MODULE__.t} | {:error, :invalid}
   def decode(<<0, 0, 0, 0>>), do: false
   def decode(<<0, 0, 0, 1>>), do: true
   def decode(_), do: {:error, :invalid}
