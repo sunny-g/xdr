7f74d15997f7d487e144a6c6eb1ee1df61b4e9b6
Sunny G <sunny.gonna@gmail.com>
2017-10-19 19:38:23 -0700

    add new to Const

--- lib/xdr/type/const.ex
+++ lib/xdr/type/const.ex
@@ -1,7 +1,7 @@
 defmodule XDR.Type.Const do
-  defmacro __using__(opts) do
-    const = Keyword.get(opts, :value)
-    module = Keyword.get(opts, :type) |> Macro.expand(__ENV__)
+  defmacro __using__(spec: spec) do
+    const = Keyword.get(spec, :value)
+    module = Keyword.get(spec, :type) |> Macro.expand(__ENV__)
     module_name = Atom.to_string(module)
 
     if not module.valid?(const) do
@@ -14,6 +14,11 @@ defmodule XDR.Type.Const do
       @behaviour XDR.Type.Base
 
       defdelegate length, to: unquote(module)
+
+      def new(val \\ unquote(const))
+      def new(val) when val === unquote(const), do: {:ok, val}
+      def new(_), do: {:error, :invalid}
+
       def valid?(val), do: val === unquote(const)
 
       def encode(unquote(const)), do: {:ok, unquote(const_xdr)}
--- test/xdr/type/const_test.exs
+++ test/xdr/type/const_test.exs
@@ -1,12 +1,10 @@
 defmodule XDR.Type.ConstTest do
-  require Math
   use ExUnit.Case
   alias XDR.Type.Const
-  doctest XDR.Type.Const
 
   defmodule XDR.Type.ConstTest.DummyConst do
     # TODO: why does this require the full module name?
-    use Const, [type: XDR.Type.Int, value: 3]
+    use Const, spec: [type: XDR.Type.Int, value: 3]
   end
 
   defmodule XDR.Type.ConstTest.InvalidSpec do
@@ -14,11 +12,23 @@ defmodule XDR.Type.ConstTest do
 
     assert_compile_time_raise RuntimeError, "invalid Const spec: 2 is not a valid Elixir.XDR.Type.String", fn ->
       # TODO: why can't I use Const here?
-      use XDR.Type.Const, [type: XDR.Type.String, value: 2]
+      use XDR.Type.Const, spec: [type: XDR.Type.String, value: 2]
     end
   end
 
   alias XDR.Type.ConstTest.DummyConst
+
+  test "new" do
+    assert DummyConst.new == {:ok, 3}
+    assert DummyConst.new(3) == {:ok, 3}
+
+    assert DummyConst.new(4) == {:error, :invalid}
+    assert DummyConst.new("3") == {:error, :invalid}
+    assert DummyConst.new(false) == {:error, :invalid}
+    assert DummyConst.new(nil) == {:error, :invalid}
+    assert DummyConst.new([]) == {:error, :invalid}
+    assert DummyConst.new({}) == {:error, :invalid}
+  end
 
   test "valid?" do
     assert DummyConst.valid?(3) == true
