4470637650494721dd2b650df35a79ed3d980b51
Sunny G <sunny.gonna@gmail.com>
2017-10-19 19:38:45 -0700

    adds new/1 to XDR.Type.Base

--- lib/xdr/type/base.ex
+++ lib/xdr/type/base.ex
@@ -1,11 +1,17 @@
 defmodule XDR.Type.Base do
   @type t :: any
   @type xdr :: <<_::_*32>>
+  @type error :: atom | String.t
 
   @doc """
-  Returns the expected length (in bits) of a XDR-encoded binary of this type (sans padding)
+  Returns the expected length (in bits) of an XDR-encoded binary of this type (sans padding)
   """
   @callback length :: non_neg_integer
+
+  @doc """
+  Returns the input if it's a valid XDR module native type, or a default valid native type
+  """
+  @callback new(any) :: {:ok, native :: t} | {:error, reason :: error}
 
   @doc """
   Determines if a value is a valid XDR type
@@ -15,10 +21,12 @@ defmodule XDR.Type.Base do
   @doc """
   Encodes a native type to an XDR binary
   """
-  @callback encode(native :: t) :: {:ok, xdr :: xdr} | {:error, reason :: atom}
+  @callback encode(native :: t) :: {:ok, xdr :: xdr} | {:error, reason :: error}
 
   @doc """
   Decodes an XDR binary to a native type
   """
-  @callback decode(xdr :: xdr) :: {:ok, native :: t} | {:error, reason :: atom}
+  @callback decode(xdr :: xdr) :: {:ok, native :: t} | {:error, reason :: error}
+
+  @optional_callbacks new: 0
 end
